37d9d9dfd957212bb1a8a9d9de3854105e6640cd
jessicanwey <jessica.n.wey@gmail.com>
2024-03-14 15:56:07 -0400

    All tests are passing now

--- tests/square.test.js
+++ tests/square.test.js
@@ -5,9 +5,9 @@ describe("Square", () => {
       const square = new Square("blue", "LVW", "yellow");
       const expectedSvg =
       `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <rect width="300" height="300" fill="blue" />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="yellow">LVW</text>
-      </svg>`;
+    <rect width="300" height="300" fill="blue" />
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="yellow">LVW</text>
+    </svg>`;
       const actualSvg = square.render();
       expect(actualSvg).toEqual(expectedSvg);
     });
