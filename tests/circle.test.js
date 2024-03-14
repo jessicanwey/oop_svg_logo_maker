f43a7351eca74884cc4af686a295c187f2d61284
jessicanwey <jessica.n.wey@gmail.com>
2024-03-14 15:52:23 -0400

    two out of three shape tests working

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,11 +1,12 @@
-const Circle = require("../lib/circle.js");
+const { Circle } = require("../lib/circle.js");
 
 describe("Circle", () => {
   test("should return an svg logo with a green circle, the letters JNW in red font", () => {
     const circle = new Circle("green", "JNW", "red");
-    const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    const expectedSvg =
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
      <circle cx="150" cy="100" r="80" style="fill:green" />
-     <text x="150" y="125" font-size="60" text-anchor="middle" fill="red">JNW</text>
+     <text x="150" y="125" font-size="60" text-anchor="middle" fill="red">JNW</text> 
      </svg>`;
     const actualSvg = circle.render();
     expect(actualSvg).toEqual(expectedSvg);
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -1,11 +1,12 @@
-const Triangle = require('../lib/triangle.js');
+const { Triangle } = require('../lib/triangle.js');
 
 describe("Triangle", () => {
     test("should return an svg logo with a purple triangle, the letters RDW in green font", () => {
       const triangle = new Triangle("purple", "RDW", "green");
-      const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-       <rect width="300" height="300" fill="purple" />
-       <text x="150" y="125" font-size="60" text-anchor="middle" fill="green">RDW</text>
+      const expectedSvg =
+      `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+       <polygon points="100, 15 200, 200 0, 200" fill="purple" />
+       <text x="150" y="125" font-size="60" text-anchor="middle" fill="green">RDW</text> 
        </svg>`;
       const actualSvg = triangle.render();
       expect(actualSvg).toEqual(expectedSvg);
